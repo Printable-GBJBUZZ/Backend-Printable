158887c1d098826e3aba87b81a36a7baad0676f2
Shivam Joshi <shivamjoshi2812@gmail.com>
2025-05-08 18:21:41 +0530

    Error occuring

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,15 +1,7 @@
-const { config } = require('dotenv');
-const { drizzle } = require('drizzle-orm/node-postgres');
-const { Pool } = require('pg');
-const schema = require('./schema');
+import { config } from "dotenv";
+import { drizzle } from 'drizzle-orm/neon-http';
+import "jsr:@std/dotenv/load";
+import * as schema from '../db/schema.ts'
+config({ path: ".env" }); 
 
-config({ path: '.env' });
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-const db = drizzle(pool, { schema });
-
-module.exports = db;
+export const db = drizzle(Deno.env.get("DATABASE_URL")!,{schema});
